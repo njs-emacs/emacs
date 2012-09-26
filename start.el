dc96763bd8c673fb2065c94c1f1cc6d9702fd510
nick <nick@flamingfly.com>
2012-09-26 21:28:48 +0100

    use canonical path generation functions, not hard coded paths

--- start.el
+++ start.el
@@ -2,18 +2,23 @@
 ; select the current environment
 
 (frame-default-height)
-
+;(find-file "e:/aa")
+;(find-file (daily-path ".emacs.el"))
+;(find-file-other-window "e:/drs/.emacs.el")
+;(find-file (daily-path "1201/.month/.emacs.el"))
 (top-level)
 
+(find-file "f:/a/apache/spix/.emacs.el")
 (find-file (concat user-emacs-home "/yas.el"))
 
-(find-file (home-path "daily/.emacs.el"))
+(find-file (daily-path ".emacs.el"))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(find-file (home-path "daily/1112/.month/.emacs.el"))
-(find-file (home-path "daily/1111/.month/.emacs.el"))
-(find-file (home-path "daily/1110/.month/.emacs.el"))
-(find-file (home-path "daily/1109/.month/.emacs.el"))
+(find-file (daily-path "1112/.month/.emacs.el"))
+(find-file (daily-path "1111/.month/.emacs.el"))
+(find-file (daily-path "1110/.month/.emacs.el"))
+(find-file (daily-path "1109/.month/.emacs.el"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(find-file (home-path "daily/1004/nose/.emacs.el"))
-(find-file (home-path "daily/1005/ovaltine/.emacs.el"))
+(find-file (daily-path "1004/nose/.emacs.el"))
+(find-file (daily-path "1005/ovaltine/.emacs.el"))
+
