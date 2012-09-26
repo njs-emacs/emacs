0a33bc1cadc93a7af41b1ce099be5dcd6f38f652
nick <nick@flamingfly.com>
2012-09-26 21:29:36 +0100

    added insert-link-yank-before-hook

--- qi-html.el
+++ qi-html.el
@@ -1,7 +1,15 @@
-(setq default-stub-html-file "~/emacs/.stub/html/default.html")
+(defun assoc-current-directory (alist)
+  (assoc-if '(lambda (x) (string-match x default-directory)) alist)
+  )
+
+(setq stub-html-file-alist nil)
+
+(setq default-stub-html-file (filename-concat user-emacs-home ".stub/html/default.html"))
 
 (defun stub-html-file ()
-  (locate-up-file ".stub.html" default-stub-html-file))
+  (or (cdr (assoc-current-directory stub-html-file-alist))
+      (locate-up-file ".stub.html" default-stub-html-file))
+  )
 
 (qi-define (control-key-vector ?h ?h)
 	   '(file-contents (stub-html-file)))
@@ -9,7 +17,11 @@
 (qb-define (control-key-vector ?h ?h)
 	   '(stub-html-file))
 
+(make-local-variable 'insert-link-yank-before-hook)
+(set-default 'insert-link-yank-before-hook nil)
+
 (defun insert-link-yank () (interactive)
+  (run-hooks 'insert-link-yank-before-hook)
   (insert (format "<a href=\"%s\"></a>" (current-kill 0)))
   (fc -4)
   )
@@ -24,3 +36,4 @@
 (global-set-key (control-key-vector ?o ?h ?i) 'insert-link-img)
 
 (qi-define "\C-h\C-i" "<img src=\"" "\">")
+
