639d2092004c9b671700499909120fbed83ede37
nick <nick@flamingfly.com>
2012-12-10 16:06:57 +0000

    retired files

--- obsolete-cl.el
+++
@@ -1,9 +0,0 @@
-(defmacro pop (sym)
-  (` (prog1 (car (,sym )) (setq (, sym) (cdr (, sym)))))
-  )
-
-(defmacro push (sym x)
-  (or (boundp sym) (set sym nil))
-  (` (setq (, sym) (cons (, x) (, sym))))
-  )
-
