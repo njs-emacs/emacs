819c38fa4ff22d7cd869919f835fe241c642223d
nick <nick@flamingfly.com>
2013-10-02 13:02:56 +0100

    local cleanup

--- start.el
+++ start.el
@@ -2,12 +2,19 @@
 ; select the current environment
 
 (frame-default-height)
+
 ;(find-file "e:/aa")
 ;(find-file (daily-path ".emacs.el"))
 ;(find-file-other-window "e:/drs/.emacs.el")
 ;(find-file (daily-path "1201/.month/.emacs.el"))
 ;(find-file "e:/daily/1209/25/proxy")
+;(find-file "e:/_/L/lily")
+;(find-file "e:/_/R/R")
+;(find-file "e:/_/T/torrent/deluge/tc")
+
 (top-level)
+
+(find-file "e:/daily/1212/pi/.emacs.el")
 
 (find-file "f:/a/apache/spix/.emacs.el")
 (find-file (concat user-emacs-home "/yas.el"))
@@ -23,3 +30,4 @@
 (find-file (daily-path "1004/nose/.emacs.el"))
 (find-file (daily-path "1005/ovaltine/.emacs.el"))
 
+(find-file (daily-path "1212/pi/.emacs.el"))
