4c442e05a2af5961d8677fcdd771bd94c7fad31d
nicko <nick@boo>
2014-01-12 13:17:19 +0000

    changes for emacs outer environmental stuff now we have symbolic links and emacs 24

--- .wuft/emacs.00--system-type-windows-nt.el
+++ .wuft/emacs.00--system-type-windows-nt.el
@@ -1,3 +1,13 @@
+(defvar emacs-exec-dir
+  "d:/e/emacs"
+  "The directory which holds the emacs environment")
+
+(defvar emacs-lisp-dir
+  (concat emacs-exec-dir "lisp")
+  "The directory which holds the emacs lisp files")
+
+(defun // (x) (concat "//" x))
+
 (setq bash-bin-root
   (filename-concat 
    (or (getenv "cgw")
@@ -5,7 +15,5 @@
    "bin"
    ))
 
-(setq emacs-exec-dir "d:/e/emacs-23/emacs")
-(setq emacs-lisp-dir (concat emacs-exec-dir "/lisp"))
 (setq bash-file-name (format "%s/bash.exe" bash-bin-root))
 (setenv "ESHELL" bash-file-name)
