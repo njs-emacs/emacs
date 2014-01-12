516f5f6d31ba252a8b26ead31dc0d548beafe270
nicko <nick@boo>
2014-01-12 13:26:13 +0000

    matching // function for linux

--- .wuft/emacs.00--system-type-gnu-linux.el
+++ .wuft/emacs.00--system-type-gnu-linux.el
@@ -1,5 +1,7 @@
 (setq bash-bin-root "/bin")
 
+(defun // (x) (concat "/" x))
+
 (setq emacs-exec-dir "/usr/bin")
 (setq emacs-lisp-dir "/usr/share/emacs/23.1/lisp")
 (setenv "ESHELL" (format "%s/bash" bash-bin-root))
