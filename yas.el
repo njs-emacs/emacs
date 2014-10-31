b1de6a48891187e3c3c92abcc125d0b5815777c6
nick <nick@boo>
2014-10-31 19:06:32 +0000

    experimental dabbling

--- yas.el
+++ yas.el
@@ -1,14 +1,26 @@
-(let ((load-path (cons (concat user-emacs-home "/packages/yasnippet-0.6.1c") load-path)))
+;(setq yas-load-path (concat user-emacs-home "/packages/yasnippet-0.6.1c"))
+(setq yas-load-path (// "boo/e/.p/yasnippet"))
+
+(let ((load-path (cons yas-load-path load-path)))
   (require 'yasnippet)
   )
 
-(add-mode "\\.ys$" 'snippet-mode)
+(setq yas-my-dir (concat user-emacs-home "/yas"))
+(setq yas-snippet-dirs yas-my-dir)
 
-(setq yas/my-dir (concat user-emacs-home "/yas"))
+(setq yas-snippet-dirs yas-my-dir)
+
+;(setq yas/root-directory (list yas/my-dir))
+;(yas/load-directory yas/my-dir)
+
+(top-level)
+
+(yas-global-mode)
+(yas-minor-mode)
+(yas-reload-all)
+;(add-mode "\\.ys$" 'snippet-mode)
 (qb-define (kbd "C-e C-y") (car yas/root-directory))
 
-(setq yas/root-directory (list yas/my-dir))
-(yas/load-directory yas/my-dir)
 
 (yas/initialize)
 
@@ -17,3 +29,20 @@
 (describe-function 'yas/expand-snippet)
 
 (menu-bar-mode)
+
+
+pim
+
+test (setq } ) ; OK!
+field with calculated default 21/02/2014 16:34:03
+<>}
+test
+pud
+
+
+yas/root-directory
+
+(symbol-macrolet
+
+yas-snippet-dirs
+yas/root-directory
