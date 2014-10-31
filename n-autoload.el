49e6fd276eb4cfe33299e86d8fb39bf291fc89ae
nick <nick@boo>
2014-10-31 17:52:59 +0000

    rename files with same name as builtin modules

---
+++ n-autoload.el
@@ -0,0 +1,54 @@
+(autoload 'todo-mode "todo")
+
+(defmacro mapautoload (file &rest syms)
+  (progn (mapcar '(lambda (x) (autoload x file nil t)) syms) nil)
+)
+
+(autoload 'hide "hide.el" "" t)
+(autoload 'time-string "time-string.el" "" t)
+
+(autoload 'query "query")
+
+(mapautoload "replace.el"
+  buffer-replace
+  buffer-replace-lines
+  )
+
+(mapautoload "shell.el"
+  call-shell
+  shell-output
+  )
+
+(mapautoload "marker.el"
+  define-key-marker
+  )
+
+(mapautoload "c-gen.el"
+  c-func
+  c-in
+  c-block
+  )
+
+(mapautoload "flash.el"
+  flash
+  flash-once
+  )
+
+(mapautoload "random"
+  random-mod
+  random-rng
+  random-p
+  random-nth)
+
+(load "autoload/dos.el" t t)
+
+(load-overrides "autoload")
+
+;(define-key global-map "\M-g" 'gud-quick)
+;(autoload 'gud-quick "gud")
+
+(autoload 'makefile-mode "make-mod")
+
+;; needs rewrite for compatability with real sql modes
+
+; (autoload 'sqlint "sql")
