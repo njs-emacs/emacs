e7b4119f685b1d77062ed984ef88de1e4c80c845
nick <nick@boo>
2014-10-31 18:46:52 +0000

    php version

--- .wuft/emacs.01--system-type-windows-nt.el
+++ .wuft/emacs.01--system-type-windows-nt.el
@@ -14,7 +14,8 @@
 ;; for php.el
 
 ;(setq php-exe "d:/p/php5/php.exe")
-(setq php-exe "d:/p/php5.3/php.exe")
+;(setq php-exe "d:/p/php5.3/php.exe")
+(setq php-exe "d:/p/PHP-5.3.28/php.exe")
 
 (defun cygdrive-to-dos (string)
   (replace-regexp-in-string "/cygdrive/\\(.\\)/"
