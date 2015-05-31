6c87eda961e7dc6e6c2ba2003a0873ec6927a70c
nickstar <nick@boo>
2015-05-31 12:58:47 +0100

    extract hacking into .help folder

---
+++ .help/yas.el
@@ -0,0 +1,14 @@
+(top-level)
+
+(yas-global-mode)
+(yas-minor-mode)
+(yas-reload-all)
+
+(top-level)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(describe-function 'yas/expand-snippet)
+
+(menu-bar-mode)
+
+yas-snippet-dirs
+yas/root-directory
