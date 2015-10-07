5dd315df0f498d3bd96624026ec097ebfc2058bb
nickstar <nick@boo>
2015-10-07 17:15:51 +0100

    extended n-register

--- n-register.el
+++ n-register.el
@@ -9,10 +9,44 @@
   (define-key my-reg-keymap (kbd "C-#") 'ace-jump-line-mode)
   (define-key my-reg-keymap (kbd "C-.") 'ace-jump-buffer)
   (define-key my-reg-keymap (kbd "C-=") 'ace-window)
+  (define-key my-reg-keymap (kbd "C-w") 'ace-jump-word-mode)
 
-  (define-key my-reg-keymap (kbd "C-b") 'recentf-open-files)
+  (define-key my-reg-keymap (kbd "C-j") 'jump-to-register)
   
+  (define-key my-reg-keymap (kbd "C-b") 'recentf-open-files)
+  (define-key my-reg-keymap (kbd "C-h") nil)
+  (define-key my-reg-keymap (kbd "C-c") 'command-history)
+
+  (define-key my-reg-keymap (kbd "C-n") 'mc/mark-next-like-this)
+  (define-key my-reg-keymap (kbd "C-p") 'mc/mark-previous-like-this)
+  (define-key my-reg-keymap (kbd "C-s") 'mc/edit-lines)
   )
 
 (my-reg-init)
 
+; Global Bindings Starting With C-x r:
+;
+;	C-@	point-to-register
+;	ESC	Prefix Command
+;	SPC	point-to-register
+;	+	increment-register
+;	N	rectangle-number-lines
+;	b	bookmark-jump
+;	c	clear-rectangle
+;	d	delete-rectangle
+;	f	frameset-to-register
+;	g	insert-register
+;	i	insert-register
+;	j	jump-to-register
+;	k	kill-rectangle
+;	l	bookmark-bmenu-list
+;	m	bookmark-set
+;	n	number-to-register
+;	o	open-rectangle
+;	r	copy-rectangle-to-register
+;	s	copy-to-register
+;	t	string-rectangle
+;	w	window-configuration-to-register
+;	x	copy-to-register
+;	y	yank-rectangle
+;	C-SPC	point-to-register
