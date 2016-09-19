4e722d58b65bf89116fae06aed7424e8901cef75
nickstar <nick@boo>
2016-09-19 19:10:35 +0100

    create comment block from tail of file

--- n-mc.el
+++ n-mc.el
@@ -19,22 +19,23 @@
 
 (global-set-key (kbd "s-<mouse-1>") 'mc/add-cursor-on-click)
 
-(top-level)
+;;(top-level)
 
-(describe-bindings)
+;;(describe-bindings)
 
-(describe-bindings (kbd "C-#"))
-(describe-bindings (kbd "C-'"))
-
-C-,		free
-C-.		free
-C-]		free
-C-;		er/expand-region
-C-#		Prefix Command
-C-'		Prefix Command
-C-[ ===> ESC
-
-(describe-function 'set-transient-map)
-
-(require 'phi-search)
-(global-set-key (kbd "C-# C-s") 'phi-search)
+;; (describe-bindings (kbd "C-#"))
+;; (describe-bindings (kbd "C-'"))
+;; 
+;; C-,		free
+;; C-.		free
+;; C-]		free
+;; C-;		er/expand-region
+;; C-#		Prefix Command
+;; C-'		Prefix Command
+;; C-[ ===> ESC
+;; 
+;; (describe-function 'set-transient-map)
+;; 
+;; (require 'phi-search)
+;; (global-set-key (kbd "C-# C-s") 'phi-search)
+;; 
