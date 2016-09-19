7e48cd758575627ee4a7b4823ec2018197b69c4a
nickstar <nick@boo>
2016-09-19 19:08:25 +0100

    use ctrl-hash-map instead of mapping keys explicitly

--- c-hash-map.el
+++ c-hash-map.el
@@ -1,6 +1,12 @@
 ;;
 
-(global-set-key (kbd "C-# SPC") 'mark-word)
-(global-set-key (kbd "C-# C-SPC") 'mark-sexp)
-(global-set-key (kbd "C-# C-s") 'magit-update-index)
-(global-set-key (kbd "C-# C-d") 'pending-delete-mode)
+(setq ctrl-hash-map (make-sparse-keymap))
+(global-set-key (kbd "C-#") ctrl-hash-map)
+
+(define-key ctrl-hash-map (kbd "SPC") 'mark-word)
+(define-key ctrl-hash-map (kbd "C-SPC") 'mark-sexp)
+(define-key ctrl-hash-map (kbd "C-s") 'magit-update-index)
+(define-key ctrl-hash-map (kbd "C-d") 'pending-delete-mode)
+
+(define-key ctrl-hash-map [C-home] 'beginning-of-defun)
+(define-key ctrl-hash-map [C-end] 'end-of-defun)
