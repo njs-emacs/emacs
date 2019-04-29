acf3fb12c3a04eeec7107ec8cc9ed0903cd6cac7
nickstar <nick@boo>
2019-04-29 12:04:34 +0100

    remove get-buffer-window

--- .wuft/init--system-type-windows-nt.el
+++ .wuft/init--system-type-windows-nt.el
@@ -2,10 +2,8 @@
   (append '(".err" ".obj" ".o32") completion-ignored-extensions)
   )
 
-(fset 'get-buffer-window
-      (list 'lambda '(buf &optional frame)
-	    (list 'funcall
-		  (symbol-function 'get-buffer-window) 'buf (or 'frame t))))
+;;; fset 'get-buffer-window probably obsolete now.
+;;; was needed because of problems with optional args
 
 (defun get-buffer-window-any-frame (buf)
   (car (delete nil (mapcar '(lambda (x) (get-buffer-window buf x))
