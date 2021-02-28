fdd6f8c9b46f3804316106d6e67df4653d624e75
nickstar <nick@otzo.org>
2021-02-28 14:36:14 +0000

    moved multiple cursor (c-comma) keys to keys/c-comma.el

---
+++ keys/c-comma.el
@@ -0,0 +1,23 @@
+(setq c-comma-map (make-sparse-keymap))
+(define-key global-map (kbd "C-,") c-comma-map)
+
+(define-key c-comma-map (kbd "E") 'keydef-edit-init)
+
+(define-key c-comma-map (kbd "C-.") 'mc/mark-more-like-this-extended)
+(define-key c-comma-map (kbd "C-x") 'mc/mark-all-like-this)
+(define-key c-comma-map (kbd "C-SPC") 'mc/edit-lines)
+(define-key c-comma-map (kbd "C-a") 'mc/edit-beginnings-of-lines)
+(define-key c-comma-map (kbd "C-e") 'mc/edit-ends-of-lines)
+
+;(define-key c-comma-map (kbd "C-s") 'mc/mark-all-in-region)
+(define-key c-comma-map (kbd "C-s") 'mc/mark-all-in-region-regexp)
+
+(define-key c-comma-map (kbd "C-n") 'mc/mark-next-like-this)
+(define-key c-comma-map (kbd "C-p") 'mc/mark-previous-symbol-like-this)
+(define-key c-comma-map (kbd "C-,") 'mc/mark-all-dwim)
+
+(define-key c-comma-map (kbd "C-o") 'mc--mark-symbol-at-point)
+
+;;; !!!!
+(global-set-key (kbd "s-,") c-comma-map)
+
