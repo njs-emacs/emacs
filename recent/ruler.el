18e31e50c124f70482b0c716372edc2ce54f6586
nickstar <nick@otzo.org>
2021-03-12 10:08:11 +0000

    add my-column-ruler (better solutions available, but this is mine)

---
+++ recent/ruler.el
@@ -0,0 +1,19 @@
+(defun my-column-ruler (width)
+   "Display temp ruler at point."
+   (interactive `(,(+ (window-hscroll) (window-width))))
+   (momentary-string-display
+    (let* ((iterations (/ (1- width) 10))
+           (result1 "|...|....|")
+           (result2 "1   5   10")
+           (inc1 "....|....|")
+           (inc2 "        %d0")
+           (i 1))
+      (while  (<= i iterations)
+        (setq i (1+ i))
+        (setq result1 (concat result1 inc1))
+        (setq result2 (concat result2 (substring (format inc2 i) -10))))
+      (concat (substring result2 0 width) "\n"
+              (substring result1 0 width) "\n"))
+    (line-beginning-position)
+    nil "[space] Clears ruler"))
+
