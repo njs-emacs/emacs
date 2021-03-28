e399a1adbb18b0db9e68947002e387310bdd9e2b
nickstar <nick@otzo.org>
2021-03-28 18:39:04 +0100

    improvements to go-boo.el

--- recent/go-boo.el
+++ recent/go-boo.el
@@ -10,22 +10,45 @@
 (defvar go-boo-prev "main")
 (defvar go-boo-script "boo.pl")
 
+(make-local-variable 'go-boo-script)
+
+(defvar-local go-boo-env nil "Extra buffer-local arg for boo.pl")
+
 (defun go-boo (fun &optional loop)
   (interactive "sFun: \nP")
   (cond ((equal fun "") (setq fun go-boo-prev)))
   (setq go-boo-prev fun)
   (let* ((loop (cond (loop 1) (0)))
 	 (script (locate-up-file go-boo-script))
+	 start end cmd
 	 )
     (cond
      (script
-      (copy-region-as-kill (region-beginning) (region-end))
-      (shell-command-on-region
-       (region-beginning) (region-end)
-       (format "perl %s --fun=%s --loop=%s --mode=%s --file=\"%s\""
-	       script fun loop major-mode (buffer-file-name))
-       nil t
+      (cond
+       ((region-active-p)
+	(setq start (region-beginning) end (region-end))
+	)
+       ((looking-at "^$")
+	(setq start (point) end (point))
+	)
+       ((bolp)
+	(setq start (point) end (point$))
+	)
+       (t
+	(setq start (point) end (point$))
+	)
        )
+      (copy-region-as-kill start end)
+      (setq cmd
+	(format "perl %s --fun=%s --loop=%s --mode=%s --file=\"%s\""
+		script fun loop major-mode (buffer-file-name))
+	)
+      (cond
+       (go-boo-env
+	(setq cmd (concat cmd (format " --env=\"%s\"" go-boo-env)))
+	)
+       )
+      (shell-command-on-region start end cmd nil t)
       )
      ((error "no script found"))
      )
