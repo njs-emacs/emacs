1c616c195d1f884bd51fb406a3ac53884bb2b125
nickstar <nick@otzo.org>
2021-04-23 14:48:34 +0100

    rewrite minibuffer-dir stuff

--- mini.el
+++ mini.el
@@ -1,48 +1,43 @@
+(defvar minibuffer-dir-map nil "Minibuffer directory shortcut map")
+(setq minibuffer-dir-map (make-sparse-keymap))
+
 (defun minibuffer-reset (s)
   (bol)
   (kill-line 1)
   (insert s))
 
-(setq minibuffer-dir-map nil)
-
 (defun minibuffer-dir-try () (interactive)
-  (let* ((k (char-to-string (logand (aref (this-command-keys) 0) 127))))
-    (minibuffer-reset (eval (gget minibuffer-dir-map k)))
+  (let* ((x (lookup-key minibuffer-dir-map (this-command-keys))))
+    (cond
+     (x (minibuffer-reset x))
+     )
     )
   )
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun minibuffer-dir-key (key d)
+  (cond (d
+	 (define-key minibuffer-local-completion-map key 'minibuffer-dir-try)
+	 (define-key minibuffer-local-must-match-map key 'minibuffer-dir-try)
+	 (define-key minibuffer-dir-map key d)
+	 )
+	(t
+	 (define-key minibuffer-local-completion-map key nil)
+	 (define-key minibuffer-local-must-match-map key nil)
+	 )
+	)
+  nil
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun minibuffer-dir (k d)
-  (let ((kk (format "\e%s" k)))
-    (cond (d
-	   (define-key minibuffer-local-completion-map kk 'minibuffer-dir-try)
-	   (define-key minibuffer-local-must-match-map kk 'minibuffer-dir-try)
-	   (setq minibuffer-dir-map (gput minibuffer-dir-map k d))
-	   )
-	  (t
-	   (define-key minibuffer-local-completion-map kk nil)
-	   (define-key minibuffer-local-must-match-map kk nil)
-	   )
-	  )
-    nil
-    ))
-
-;(define-key minibuffer-local-map "\M-\C-?"
-;  '(lambda () (interactive)
-;     (while (not (= (preceding-char) ?/)) (backward-delete-char 1))))
-
-(defun minibuffer-show () (interactive)
-  (let* ((e (cdr (lookup-key minibuffer-local-completion-map "\e")))
-	 (ee (subset-if 
-	      '(lambda (x) (and (stringp (cdr x)) (file-directory-p (cdr x))))
-	      e)))
-    (show (mdolist (i ee) (format "%c\t%s\n" (car i) (cdr i))))
-    )
+  (let ((kk (format "\e%s" k))) (minibuffer-dir-key kk d))
   )
 
-(defun minibuffer-show () (interactive)
-  (apply 'show (mdolist (i minibuffer-dir-map) (format "%s\t%s\n" (car i) (cdr i))))
+(defun minibuffer-dir-show () (interactive)
+  (require 'which-key)
+  (which-key-show-full-keymap 'minibuffer-dir-map)
   )
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
