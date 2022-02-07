3ef1ee58d518131e635743f40aeb1750256d7c83
nickstar <nick@otzo.org>
2022-02-07 14:36:56 +0000

    updated org-capture samples

--- recent/2202.el
+++ recent/2202.el
@@ -2,6 +2,9 @@
   (interactive)
   (org-capture-template-add "t" '("todo" entry (file+headline "" "Tasks") "* TODO %?\n  %T"))
   (org-capture-template-add "x" '("todo" entry (file "") "* DONE %?\n  %u"))
+  (org-capture-template-add "s" '("slogan" entry (file "slogans.org") "* %?"))
+  (org-capture-template-add "p" '("projects" entry (file+headline "" "projects") "* %?"))
+  (org-capture-template-add "n" '("notes" entry (file "") "* %? %^g"))
   )
 
 ;(*2202-org-init)
