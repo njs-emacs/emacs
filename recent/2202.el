b98e1cd6bb91d5cf49b2f6ed033fffe7927a7d72
nickstar <nick@otzo.org>
2022-02-09 02:21:17 +0000

    removed extra test TODO capture item

--- recent/2202.el
+++ recent/2202.el
@@ -1,7 +1,7 @@
 (defun *2202-org-init ()
   (interactive)
   (org-capture-template-add "t" '("todo" entry (file+headline "" "Tasks") "* TODO %?\n  %T"))
-  (org-capture-template-add "x" '("todo" entry (file "") "* DONE %?\n  %u"))
+;  (org-capture-template-add "x" '("todo" entry (file "") "* DONE %?\n  %u"))
   (org-capture-template-add "s" '("slogan" entry (file "slogans.org") "* %?"))
   (org-capture-template-add "p" '("projects" entry (file+headline "" "projects") "* %?"))
   (org-capture-template-add "n" '("notes" entry (file "") "* %? %^g"))
