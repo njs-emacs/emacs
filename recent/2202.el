fd85086da6e90a241d00b69080c5b681fa0815c4
nickstar <nick@otzo.org>
2022-02-11 13:44:04 +0000

    add interesting capture type

--- recent/2202.el
+++ recent/2202.el
@@ -5,6 +5,7 @@
   (org-capture-template-add "s" '("slogan" entry (file "slogans.org") "* %?"))
   (org-capture-template-add "p" '("projects" entry (file+headline "" "projects") "* %?"))
   (org-capture-template-add "n" '("notes" entry (file "") "* %? %^g"))
+  (org-capture-template-add "i" '("interesting" entry (file "") "* %? %^g"))
   )
 
 ;(*2202-org-init)
