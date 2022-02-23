537a355f87c1070a2bff053f1a269247f22a068e
nickstar <nick@otzo.org>
2022-02-23 22:42:34 +0000

    added ket-translation stuff to recent/2202.el

--- recent/2202.el
+++ recent/2202.el
@@ -11,3 +11,21 @@
 ;(*2202-org-init)
 
 ;(apropos "org-capture-template-put")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun *2202-perl-key-translation () (interactive)
+; make A-| produce a copyright symbol
+  (define-key key-translation-map  (kbd "A-|") "©")
+
+; make copyright symbol a word constituent
+  (modify-syntax-entry 169 "w")
+
+; now we can create abbreviations and snippets with no conflicts
+; but we wouldn't want it to be too hard to insert the copyright symbol
+; A-| isn't very hard
+; but we could create a key mapping to insert and expand the snippet
+
+; this part is specific to needs
+  (local-set-key (kbd "H-1") "©a	Z")
+  )
+
