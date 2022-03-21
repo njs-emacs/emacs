8879da24dee75a0f1ab7b6ce8f248f7942273cf5
nickstar <nick@otzo.org>
2022-03-21 13:26:43 +0000

    use bash-command to invoke powershell (problems with cmdproxy)

--- ps1-mode.el
+++ ps1-mode.el
@@ -1,9 +1,17 @@
 (make-variable-buffer-local 'ps1-args)
 (set-default 'ps1-args "")
 
+(defun bash-command (cmd)
+  (shell-command (format "bash -c \"%s\"" cmd))
+  )
+
 (defun ps1-eval-buffer () (interactive)
   (save-buffer)
-  (shell-command (format "powershell -Noninteractive -Command %s %s" (buffer-file-name) ps1-args))
+;; there's a problem with cmdproxy invoking powershell
+;; if we use bash in between it works, don't know why
+
+;  (shell-command (format "powershell -Noninteractive -Command %s %s" (buffer-file-name) ps1-args))
+  (bash-command (format "powershell -Noninteractive -Command %s %s" (buffer-file-name) ps1-args))
   )
 
 (put 'ps1-mode 'eval-buffer-modal 'ps1-eval-buffer)
@@ -12,5 +20,3 @@
   (setq major-mode 'ps1-mode)
   (setq mode-name "PS1")
   )
-  
-
