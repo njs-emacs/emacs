b31736dc98170896d396c4d70494c9162d3881e6
nickstar <nick@otzo.org>
2022-04-15 14:20:33 +0100

    more code for helm find-file sorting

--- .try/helm/helm-sort.el
+++ .try/helm/helm-sort.el
@@ -1,39 +1,117 @@
-(define-advice helm-list-directory (:override (directory) add-sort-by-kind)
-  (let ((files (directory-files directory t directory-files-no-dot-files-regexp)))
-    (pcase helm-ff-initial-sort-method
-      ('nil
-       files)
-      ('newest
-       (sort files #'file-newer-than-file-p))
-      ('size
-       (sort files #'helm-ff-file-larger-that-file-p))
-      ('kind
-       (let-alist (seq-group-by #'file-directory-p files)
-         (nconc
-          .t                            ; folders
-          (sort
-           .nil                         ; files
-           (lambda (f1 f2)
-             (let ((ext1 (or (file-name-extension f1) ""))
-                   (ext2 (or (file-name-extension f2) "")))
-               (if (equal ext1 ext2)
-                   (string< f1 f2)
-                 (string< ext1 ext2)))))))))))
+(defun file-bigger-than-file-p (a b)
+  "return t if file A is bigger than file B. Directories are treated as zero length."
+  (let* ((aa (file-attribute-size (file-attributes a)))
+	 (bb (file-attribute-size (file-attributes b)))
+	 )
+    (> aa bb))
+  )
 
-(defun helm-ff-sort-by-kind ()
+
+(defun file-type-gt-file-p (f1 f2)
+  "return t if the string which describes the type for file A is \"bigger\" than the same attribute of file B."
+  (let ((ext1 (or (file-name-extension f1) ""))
+	(ext2 (or (file-name-extension f2) "")))
+    (if (equal ext1 ext2)
+	(string< f1 f2)
+      (string< ext1 ext2)))
+  )
+
+(defun helm-ff-sort-apply (files function)
+  (let ((sorted (sort files function)))
+    (cond
+     (helm-ff-sort-reverse (nreverse sorted))
+     (sorted)
+     )
+    )
+  )
+
+(define-advice helm-list-directory (:override (directory) add-sort-by-moob)
+  (let* ((files (directory-files directory t directory-files-no-dot-files-regexp))
+	 (sfiles (pcase helm-ff-sort-method
+		   ('nil
+		    files)
+		   ('newest
+		    (helm-ff-sort-apply files #'file-newer-than-file-p))
+		   ('biggest
+		    (helm-ff-sort-apply files #'file-bigger-than-file-p))
+		   ('type
+		    (let-alist (seq-group-by #'file-directory-p files)
+		      (nconc
+		       .t                            ; folders
+		       (helm-ff-sort-apply
+			.nil                         ; files
+			#'file-type-gt-file-p
+			)))))))
+
+    sfiles
+    )
+  )
+
+(defun helm-ff-sort-refresh ()
+  (helm-force-update (helm-get-selection nil helm-ff-transformer-show-only-basename))
+  (message "Sorting by %s %s"  helm-ff-sort-method (if helm-ff-sort-reverse "(reversed)"))
+  )
+
+(defun helm-ff-sort (x)
+  (interactive "SSort by: ")
+  (unless (eq helm-ff-sort-method x)
+    (setq helm-ff-sort-method x)
+    (helm-ff-sort-refresh)
+    )
+  )
+
+(defun helm-ff-sort-type ()
   (interactive)
-  (unless (eq helm-ff-initial-sort-method 'kind)
-    (setq helm-ff-initial-sort-method 'kind)
-    (helm-force-update (helm-get-selection nil helm-ff-transformer-show-only-basename)))
-  (message "Sorting by kind"))
+  (helm-ff-sort 'type)
+  )
 
-(put 'helm-ff-sort-by-kind 'helm-only t)
+(defun helm-ff-sort-newer ()
+  (interactive)
+  (helm-ff-sort 'newest)
+  )
 
-(define-key helm-find-files-map (kbd "S-<f4>") #'helm-ff-sort-by-kind)
+(defun helm-ff-sort-biggest ()
+  (interactive)
+  (helm-ff-sort 'biggest)
+  )
 
+(defun helm-ff-sort-off ()
+  (interactive)
+  (helm-ff-sort nil)
+  )
 
-(setq helm-ff-initial-sort-method 'size)
-(setq helm-ff-initial-sort-method 'newest)
-(setq helm-ff-initial-sort-method 'kind)
+(defun helm-ff-sort-reverse-toggle ()
+  (interactive)
+  (setq helm-ff-sort-reverse (not helm-ff-sort-reverse))
+  (helm-ff-sort-refresh)
+  )
+
+(defvar helm-ff-sort-reverse nil)
+(defvar helm-ff-sort-method nil)
+
+(put 'helm-ff-sort-type 'helm-only t)
+(put 'helm-ff-sort-off 'helm-only t)
+(put 'helm-ff-sort-newer 'helm-only t)
+(put 'helm-ff-sort-biggest 'helm-only t)
+(put 'helm-ff-sort-reverse-toggle 'helm-only t)
+
+(define-key helm-find-files-map (kbd "H-q r") #'helm-ff-sort-reverse-toggle)
+
+(define-key helm-find-files-map (kbd "H-q q") #'helm-ff-sort-off)
+(define-key helm-find-files-map (kbd "H-q a") #'helm-ff-sort-newer)
+(define-key helm-find-files-map (kbd "H-q t") #'helm-ff-sort-type)
+(define-key helm-find-files-map (kbd "H-q s") #'helm-ff-sort-biggest)
+
+;(setq helm-ff-sort-method 'biggest)
+;(setq helm-ff-sort-method 'newest)
+;(setq helm-ff-sort-method 'type)
+
 
 ;
+
+;;; (apropos "remove.*advice")
+;;; (apropos "advice.*remove")
+;;; (apropos "advice")
+;;; 
+;;; (advice-mapc 'print 'helm-list-directory)
+;;; (advice-remove 'helm-list-directory 'helm-list-directory@add-sort-xxx)
