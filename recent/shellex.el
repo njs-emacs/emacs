31524a0fd9304d7b9df87ca04447f08617f5637f
nickstar <nick@otzo.org>
2023-03-27 18:02:48 +0100

    allow more control over output buffer

--- recent/shellex.el
+++ recent/shellex.el
@@ -1,43 +1,67 @@
 (defun shell-execute-text (s &rest plist)
+
   (let* ((nvc-global-enable nil)
 	 (shell-program (or (plist-get plist :shell-program) "bash"))
-	 (file-name (or (plist-get plist :file-name)
-			(format "%s/bash-%d.tmp" (temporary-file-directory) (emacs-pid))))
 	 (output-buffer-name (or (plist-get plist :output-buffer-name) "*shell-execute*"))
 	 (output-buffer (get-buffer-create output-buffer-name))
 	 shell-command
 	 shell-output
+	 (exec-file-name (or (plist-get plist :file-name)
+			     (format "%s/bash-%d.tmp" (temporary-file-directory) (emacs-pid))))
 	 exec-buffer
+	 temp-output-buffer
 	 )
 
     (cond
      ((string= shell-program "powershell")
-      (setq file-name (slash-back (format "%s.ps1" file-name)))
-      (setq shell-command (format "%s %s" shell-program file-name))
+      (setq exec-file-name (slash-back (format "%s.ps1" exec-file-name)))
+      (setq shell-command (format "%s %s" shell-program exec-file-name))
       )
      ((string= shell-program "cmd")
-      (setq file-name (slash-back (format "%s.bat" file-name)))
-      (setq shell-command (format "%s /c %s" shell-program file-name))
+      (setq exec-file-name (slash-back (format "%s.bat" exec-file-name)))
+      (setq shell-command (format "%s /c %s" shell-program exec-file-name))
       )
      (t
-      (setq shell-command (format "%s %s" shell-program file-name))
+      (setq shell-command (format "%s %s" shell-program exec-file-name))
       )
      )
 
     (save-excursion
-      (setq exec-buffer (find-file-noselect file-name))
+      (setq exec-buffer (find-file-noselect exec-file-name))
       (set-buffer exec-buffer)
       (set-buffer-file-coding-system 'unix)
       (erase-buffer)
       (insert s)
-;      (with-suppressed-message (write-file file-name))
-      (write-region nil nil file-name nil 0)
+      (write-region nil nil exec-file-name nil 0)
       (set-buffer-modified-p nil)
       )
-    (shell-command shell-command output-buffer)
-    (setq shell-output (get-buffer-string output-buffer))
+    (setq temp-output-buffer (get-buffer-create (format " *shell-execute-%s" (format-time-string "%s%3N"))))
+    (shell-command shell-command temp-output-buffer)
+    (setq shell-output (get-buffer-string temp-output-buffer))
+    (kill-buffer temp-output-buffer)
+
+    (set-buffer output-buffer)
+    (or (plist-get plist :at-bob)
+	(plist-get plist :at-eob)
+	(plist-get plist :no-erase)
+	(erase-buffer)
+	)
+
     (cond
-     ((plist-get plist :show))
+     ((plist-get plist :at-bob) (bob))
+     ((plist-get plist :at-eob) (eob))
+     (t (bol))
+     )
+
+    (insert shell-output)
+
+    (cond
+     ((plist-get plist :show)
+      (display-buffer-use-some-window output-buffer nil)
+      )
+     ((plist-get plist :display)
+      (display-buffer output-buffer (plist-get plist :display) (plist-get plist :display-frame))
+      )
      ((plist-get plist :kill)
       (kill-buffer output-buffer)
       )
