2501a8785f324ad36c0086bbf99d5ecb24f39580
nickstar <nick@otzo.org>
2023-07-22 20:22:37 +0100

    improve shell-execute-text

--- recent/shellex.el
+++ recent/shellex.el
@@ -1,4 +1,4 @@
-(defun shell-execute-text (s &rest plist)
+(defun shell-execute-text* (s plist)
 
   (let* ((nvc-global-enable nil)
 	 (shell-program (or (plist-get plist :shell-program) "bash"))
@@ -7,15 +7,17 @@
 	 shell-command
 	 shell-output
 	 (exec-file-name (or (plist-get plist :file-name)
-			     (format "%s/bash-%d.tmp" (temporary-file-directory) (emacs-pid))))
+			     (format "%s/%s-%d.tmp" (temporary-file-directory) shell-program (emacs-pid))))
 	 exec-buffer
 	 temp-output-buffer
 	 )
 
     (cond
      ((string= shell-program "powershell")
-      (setq exec-file-name (slash-back (format "%s.ps1" exec-file-name)))
-      (setq shell-command (format "%s %s" shell-program exec-file-name))
+      (let ((shell-args (or (plist-get plist :shell-args) "")))
+	(setq exec-file-name (slash-back (format "%s.ps1" exec-file-name)))
+	(setq shell-command (format "%s -Noninteractive -Command %s" shell-program exec-file-name))
+	)
       )
      ((string= shell-program "cmd")
       (setq exec-file-name (slash-back (format "%s.bat" exec-file-name)))
@@ -35,6 +37,7 @@
       (write-region nil nil exec-file-name nil 0)
       (set-buffer-modified-p nil)
       )
+
     (setq temp-output-buffer (get-buffer-create (format " *shell-execute-%s" (format-time-string "%s%3N"))))
     (shell-command shell-command temp-output-buffer)
     (setq shell-output (get-buffer-string temp-output-buffer))
@@ -69,6 +72,10 @@
      )
     shell-output
     )
+  )
+
+(defun shell-execute-text (s &rest plist)
+  (save-excursion (shell-execute-text* s plist))
   )
 
 (defun shell-execute-region-text (start end)
