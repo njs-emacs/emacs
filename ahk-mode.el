82399db5490cc3eb5ab8c387dc1cf592af6395c5
nickstar <nick@otzo.org>
2023-07-22 20:16:10 +0100

    added ahk buffer eval

--- ahk-mode.el
+++ ahk-mode.el
@@ -1,2 +1,30 @@
-(defun ahk-mode () (interactive)
+(setq ahk-exec-program "d:/a/autohotkey/autohotkey.exe")
+
+(defun ahk-mode ()
+  (interactive)
+  (setq major-mode 'ahk-mode)
+  (setq mode-name "AHK")
   )
+
+(put 'ahk-mode 'eval-buffer-modal 'ahk-buffer-eval)
+
+(defun ahk-buffer-eval-shell ()
+  (shell-command
+   (format "%s \"%s\"" ahk-exec-program (buffer-file-name))
+   )
+  )
+
+(defun ahk-buffer-eval-compile ()
+  (compile
+   (format "%s \"%s\"" ahk-exec-program (buffer-file-name))
+   )
+  )
+
+(defun ahk-buffer-eval ()
+  (save-buffer)
+  (ahk-buffer-eval-shell)
+  )
+
+(add-mode "\\.ahk$" 'ahk-mode)
+
+(defun ahk-execute-string (s))
