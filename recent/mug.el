6e4390a5e3e43d1e65429fb5f494539144d3e256
nickstar <nick@otzo.org>
2023-08-02 19:04:52 +0100

    added doc strings

--- recent/mug.el
+++ recent/mug.el
@@ -9,6 +9,7 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun alist-put (list tag val)
+  "Like plist-put but acts on alists. Should really be built-in."
   (let ((cell (assoc tag list)))
     (cond
      (cell (setcdr cell val))
@@ -17,17 +18,36 @@
     list)
   )
 
-(defmacro sxp (&rest body) `(save-excursion ,@body (point)))
+(defmacro sxp (&rest body)
+  "Like save-excursion, but returns where point was at the end of theb ody execution."
+  `(save-excursion ,@body (point)))
 
-(defun point^ () (sxp (beginning-of-line)))
-(defun point$ () (sxp (end-of-line)))
+(defun point^ ()
+  "The value of (point) at the start of the line."
+ (sxp (beginning-of-line)))
+
+(defun point$ ()
+  "The value of (point) at the end of the line."
+ (sxp (end-of-line)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun bit-set-p (v mask &optional match)
   (= (logand v mask) (or match mask))
   )
 
 (defun interactive-arg-read (spec)
-  (call-interactively `(lambda (&rest args) (interactive ,spec) args))
+  "Simulate the process of gathering args for an interactive function."
+  (cond
+   ((and (symbolp spec) (fboundp spec))
+    (interactive-arg-read (cdr (interactive-form spec)))
+    )
+   ((stringp spec)
+    (call-interactively `(lambda (&rest args) (interactive ,spec) args))
+    )
+   ((listp spec)
+    (call-interactively `(lambda (&rest args) (interactive ,@spec) args))
+    )
+   )
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
