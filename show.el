f318f590604f312e55dac7a4587ce6a841a4e9c7
nickstar <nick@otzo.org>
2023-08-29 18:26:43 +0100

    extend show.el - allow append to *show* buffer

--- show.el
+++ show.el
@@ -1,11 +1,18 @@
 (require 'reap)
 
-(defun show-buf (buf contents &optional print)
+(defun show-buf (buf contents &optional print append)
+  "Display output in a 'show' buffer."
   (or print (setq print 'prin))
   (or (get-buffer-window-any-frame buf) (display-buffer buf))
   (sx 
    (set-buffer buf)
-   (erase-buffer)
+   (cond
+    (append
+     (end-of-buffer)
+     (and (stringp append) (insert append))
+     )
+    (t (widen) (kill-region (point-min) (point-max)))
+    )
    (dolist (i contents) (insert (funcall print i)))
    (setq major-mode 'show)
    (setq mode-name "show")
@@ -14,11 +21,11 @@
   buf
   )
 
-(setq show-buffer-name-function 'show-buffer-name-function)
-
 (defun show-buffer-name-function ()
   "*show*"
   )
+
+(setq show-buffer-name-function 'show-buffer-name-function)
 
 (defun show (&rest contents)
   (let ((buf (make-buffer (funcall show-buffer-name-function))))
